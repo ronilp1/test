2c25fa40ddc17cefdf7b068788ab7314665c5f0f
rrprasad <rrprasad@amazon.com>
2023-07-17 16:03:10 +1200

    first commit

---
+++ test.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c574c8"
+  instance_type = "t2.micro"
+  key_name      = "your-key-name"
+
+  tags = {
+    Name = "example-instance"
+  }
+}
