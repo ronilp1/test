385f3a294017663694e4f8bb8132084ca71e4efb
rrprasad <rrprasad@amazon.com>
2023-07-19 10:08:07 +1200

    test

--- sources/aft-customizations-repos/aft-global-customizations/terraform/test.tf
+++ sources/aft-customizations-repos/aft-global-customizations/terraform/test.tf
@@ -6,6 +6,8 @@ resource "aws_instance" "example" {
   ami           = "ami-0b94777c7d8bfe7e3"
   instance_type = "t2.micro"
 
+  subnet_id = "subnet-05f0d66db2a906ed9" # <--- Add this line. Replace "your-subnet-id" with your actual subnet ID.
+
   tags = {
     Name = "example-instance"
   }
