6b15a93a0aa6dbd63a723187df28081bdda1608d
rrprasad <rrprasad@amazon.com>
2023-07-19 14:45:43 +1200

    test

--- sources/aft-customizations-repos/aft-global-customizations/terraform/test.tf
+++ sources/aft-customizations-repos/aft-global-customizations/terraform/test.tf
@@ -2,13 +2,4 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0b94777c7d8bfe7e3"
-  instance_type = "t2.micro"
-
-  subnet_id = "subnet-05f0d66db2a906ed9" # <--- Add this line. Replace "your-subnet-id" with your actual subnet ID.
-
-  tags = {
-    Name = "example-instance-2"
-  }
-}
+resource "aws_default_vpc" "default" {}
