205e79351fadf46c991948cf570555fc0b247806
rrprasad <rrprasad@amazon.com>
2023-07-20 09:31:30 +1200

    test

--- sources/aft-customizations-repos/aft-global-customizations/terraform/test.tf
+++ sources/aft-customizations-repos/aft-global-customizations/terraform/test.tf
@@ -2,6 +2,6 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_vpc" "testvpc" {
-  cidr_block = "10.3.0.0/16"
+resource "aws_vpc" "testvpcsecond" {
+  cidr_block = "10.2.0.0/16"
 }
