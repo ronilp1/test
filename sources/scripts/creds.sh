4a63c9bd9c80938361def139ad2c26ebd8420e7a
rrprasad <rrprasad@amazon.com>
2023-07-19 14:58:15 +1200

    test

--- sources/scripts/creds.sh
+++ sources/scripts/creds.sh
@@ -31,16 +31,10 @@ function assume_role_and_write_credentials {
 
 # Variables
 AWS_PARTITION="aws"
-TARGET_ACCOUNT_IDS=$1 # Receive the target account IDs as an argument
+TARGET_ACCOUNT_ID=${1} # Replace with your target account ID
 ROLE_SESSION_NAME="CodeBuild-Session"
 TARGET_ROLE_NAME="allow-code-commit-account" # Name of the role created in target account
+TARGET_ROLE_ARN="arn:${AWS_PARTITION}:iam::${TARGET_ACCOUNT_ID}:role/${TARGET_ROLE_NAME}"
 
-#IFS=',' read -ra ACCOUNTS <<< "$TARGET_ACCOUNT_IDS"
-IFS=' ' read -ra ACCOUNTS <<< "$TARGET_ACCOUNT_IDS"
-
-for ACCOUNT_ID in "${ACCOUNTS[@]}"; do
-    echo "Processing account: $ACCOUNT_ID"
-    TARGET_ROLE_ARN="arn:${AWS_PARTITION}:iam::${ACCOUNT_ID}:role/${TARGET_ROLE_NAME}"
-    # Assume the role in the target account and write the credentials
-    assume_role_and_write_credentials "${TARGET_ROLE_ARN}" "codebuild-target-$ACCOUNT_ID"
-done
+# Assume the role in the target account and write the credentials
+assume_role_and_write_credentials "${TARGET_ROLE_ARN}" "codebuild-target"
