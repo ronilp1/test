0fae632312dfaa1873b438acda9a9637582f7d62
rrprasad <rrprasad@amazon.com>
2023-07-20 16:27:14 +1200

    test

--- codebuild_batches.py
+++ codebuild_batches.py
@@ -4,15 +4,21 @@ import subprocess
 account_ids = [
     # Replace these with your list of account IDs
     "719797195793", "939115282395", "530397177630", "502771900580"
-    
 ]
 
-# Split the account IDs into groups of 10
+# Split the account IDs into groups of 2
 account_id_groups = [account_ids[i:i + 2] for i in range(0, len(account_ids), 2)]
 
-for group in account_id_groups:
+print(f"Account ID Groups: {account_id_groups}")
+
+for index, group in enumerate(account_id_groups):
     target_account_ids = " ".join(group)
     command = f"aws codebuild start-build --project-name create-build-project --environment-variables-override name=TARGET_ACCOUNT_IDS,value=\"{target_account_ids}\",type=PLAINTEXT"
     
     # Call the AWS CLI command
-    subprocess.call(command, shell=True)
+    print(f"Running Command for Group {index + 1}: {command}")
+    exit_status = subprocess.call(command, shell=True)
+    print(f"Command exit status: {exit_status}")
+
+    # Let's add a pause here for debugging
+    input("Press Enter to continue to the next group...")
--- sources/aft-customizations-repos/aft-global-customizations/terraform/test.tf
+++ sources/aft-customizations-repos/aft-global-customizations/terraform/test.tf
@@ -2,6 +2,13 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-resource "aws_vpc" "testvpcsecond" {
-  cidr_block = "10.1.0.0/16"
+resource "aws_instance" "example" {
+  ami           = "ami-0b94777c7d8bfe7e3"
+  instance_type = "t2.micro"
+
+  subnet_id = "subnet-05f0d66db2a906ed9" # <--- Add this line. Replace "your-subnet-id" with your actual subnet ID.
+
+  tags = {
+    Name = "example-instance-3"
+  }
 }
